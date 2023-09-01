375c2a5b1289850e75f42aaac2cc2d16544fa896
oscar066 <oscarkaruga1@gmail.com>
2023-09-01 12:07:36 +0300

    :construction: Created a Starter template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { AboutComponent } from './components/about/about.component';
+import { ServicesComponent } from './components/services/services.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  , HeaderComponent, HeroComponent, AboutComponent, ServicesComponent, ContactsComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule
